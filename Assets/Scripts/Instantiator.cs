f81b5dbc003e969bb09494dd1f96409a57d4f83d
SofiaCirelli <101577333+SofiaCirelli@users.noreply.github.com>
2022-05-09 10:40:34 -0300

    Instantiating objects certain amounts of times

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject ObjectToClone;
+    public int num;
+    int contador;
+
+    public void CloneObject()
+    {
+        int contador = num;
+        while (contador > 0)
+        {
+        Instantiate(ObjectToClone);
+            contador--;
+        }      
+    }
+}
